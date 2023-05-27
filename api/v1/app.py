7cb9117e19a4ebd733b0fb8e27660aa1d5ffb846
nathan-assefa <aasamnew@gmail.com>
2023-05-27 07:47:03 +0000

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,12 +21,12 @@ when the routes are defined within the modules using the app_views blueprint.
 from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
